701a167082999da822c1df8414ed1dc811cc5bb8
Kudretizade <104269357+Kudretizade@users.noreply.github.com>
2022-08-18 22:36:07 +0400

    Movement controller isnt works

--- 4secim/Assets/App/Scripts/PlayerController.cs
+++ 4secim/Assets/App/Scripts/PlayerController.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody rb;
-    public float speed;
-    public float dirX, dirZ;
+    private Rigidbody rb;
+    private float moveSpeed;
+    private float dirX, dirZ;
 
     private void Start()
     {
-        speed = 3f;
+        moveSpeed = 5f;
         rb = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
-        dirX = Input.GetAxis("Horizontal") * speed;
-        dirZ = Input.GetAxis("Vertical") * speed;
+        dirX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        dirZ = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector3 (dirX, rb.velocity.y, dirZ);
+        rb.velocity = new Vector3(dirX, rb.velocity.y, dirZ);
     }
 }
